101f5403ea3ac19f9dcd0f35894a3b0a12fcf9ac
K.Evtimov <kaevtimov@gmail.com>
2018-12-12 06:10:53 +0200

    added kotlin

---
+++ app/src/main/java/source/kevtimov/landlordcommunicationapp/async/AsyncSchedulerProvider.kt
@@ -0,0 +1,32 @@
+package source.kevtimov.landlordcommunicationapp.async
+
+import javax.inject.Inject
+
+import io.reactivex.Scheduler
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import source.kevtimov.landlordcommunicationapp.async.base.SchedulerProvider
+
+class AsyncSchedulerProvider @Inject
+constructor() : SchedulerProvider {
+
+    override fun background(): Scheduler {
+        return Schedulers.io()
+    }
+
+    override fun ui(): Scheduler {
+        return AndroidSchedulers.mainThread()
+    }
+
+    companion object {
+        private var instance: SchedulerProvider? = null
+
+        val schedulerProvider: SchedulerProvider
+            get() {
+                if (instance == null) {
+                    instance = AsyncSchedulerProvider()
+                }
+                return instance as SchedulerProvider
+            }
+    }
+}
